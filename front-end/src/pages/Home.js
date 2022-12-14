94bacd5b8a0c2f4c0ea40188a4c2c62d17dc0be8
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-12-14 11:59:19 +0000

    Update Home.js

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -3,8 +3,9 @@ import "../assets/css/styles.css";
 import NavBar from "../components/NavBar";
 import Hero from "../components/Hero";
 import Mint from "../components/Mint";
-import Footer from "../components/Footer";
 import Roadmap from "../components/Roadmap";
+import Faq from "../components/Faq";
+import Footer from "../components/Footer";
 
 const Home = () => {
 
